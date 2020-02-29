31562ce6ae82bfa28e135b8fc0b95c3a5ed551f1
Igor Hristov <igorhristov@sourcemxacademy.mk>
2020-02-29 16:42:02 +0100

    exercisam

--- WebsitesCodeTrain/exercism/resistor-color-duo.js
+++ WebsitesCodeTrain/exercism/resistor-color-duo.js
@@ -15,3 +15,20 @@ const COLORS1 = [
 ];
 
 console.log(decodedValue(['White','Brown','red']));
+
+const decodedValue1 = (arr) => {
+  return (COLORS1.indexOf(arr[0]) * 10 + COLORS1.indexOf(arr[1]));
+};
+
+console.log(decodedValue1(['White','Brown','red']));
+
+
+// const decodedValue3 = ([color1, color2]) =>  [...COLORS1]
+ 
+// console.log(decodedValue3(['White','Brown']));
+
+const decodedValue4 = (...[colors]) => {
+  let [color1, color2] = colors;
+    return Number(`${COLORS1.indexOf(color1)}${COLORS1.indexOf(color2)}`);
+};
+console.log(decodedValue4(['White','Brown']));
