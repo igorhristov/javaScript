14a4f3446cf0339c6c972456f508b28290f59966
igorhristov <igorhristov@sourcemxacademy.mk>
2020-03-05 23:45:57 +0100

    tboy rows on event are red or white

--- academyMX/exercise0603/script.js
+++ academyMX/exercise0603/script.js
@@ -1,4 +1,17 @@
 const runTable = async () => {
-    await createTable('asc', 'firstName');
-}
-runTable()
+  await createTable('asc', 'firstName');
+  const trNodes = document.querySelectorAll('tbody tr');
+  const arrTbodyRows = [...trNodes];
+
+  const onMouseEnter = event => {
+    event.target.style.backgroundColor = 'red';
+  };
+  const onMouseLeave = event => {
+    event.target.style.backgroundColor = 'white';
+  };
+  arrTbodyRows.forEach(btn => {
+    btn.addEventListener('mouseenter', onMouseEnter);
+    btn.addEventListener('mouseleave', onMouseLeave);
+  });
+};
+runTable();
