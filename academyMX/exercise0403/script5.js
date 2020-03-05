58e5eb3c8ddab6993954655f23d32a081820eb6c
Igor Hristov <igorhristov@sourcemxacademy.mk>
2020-03-05 12:18:02 +0100

    getuserID from TABLE exercise5

---
+++ academyMX/exercise0403/script5.js
@@ -0,0 +1,17 @@
+const RUN_TABLE = async () => {
+    await createTable();
+    const btns = document.querySelectorAll('table button');
+    const arrBtns = [...btns]
+    const onButtonClick = async (event) => {
+        const buttons = event.target;
+        console.log(buttons);
+        const btnId = buttons.getAttribute('id').replace('userId-', '')
+        console.log(btnId);
+        const user = await getUser(btnId * 1)
+        console.log(user);
+    }
+    arrBtns.forEach(btn => {
+        btn.addEventListener('click', onButtonClick);
+    })
+}
+RUN_TABLE()
