bfb88aeb3fe2df57f7ac266f1193d4f71542c05c
igorhristov <igorhristov@sourcemxacademy.mk>
2020-03-05 23:55:34 +0100

    Update createTable.js

--- academyMX/exercise0603/createTable.js
+++ academyMX/exercise0603/createTable.js
@@ -1,92 +1,91 @@
 const createElement = (type, attrs = {}) => {
-    const el = document.createElement(type);
-    
-    Object.keys(attrs).forEach(attr => {
-        el[attr] = attrs[attr];
-    });
+  const el = document.createElement(type);
 
-    return el;
-}
+  Object.keys(attrs).forEach(attr => {
+    el[attr] = attrs[attr];
+  });
+
+  return el;
+};
 
 const createTable = async (sortOrder = 'asc', sortBy = 'id') => {
-    const d = document;
-    const users = await getUsers()
-        .then(users => {
-            return users.sort((a, b) => {
-                const by1 = a[sortBy];
-                const by2 = b[sortBy];
-                return sortOrder === 'desc' 
-                    ? by2 > by1 ? 1 : -1 
-                    : by1 > by2 ? 1 : -1;
-            })
+  const d = document;
+  const users = await getUsers().then(users => {
+    return users.sort((a, b) => {
+      const by1 = a[sortBy];
+      const by2 = b[sortBy];
+      return sortOrder === 'desc' ? (by2 > by1 ? 1 : -1) : by1 > by2 ? 1 : -1;
+    });
+  });
+  const columns = {
+    id: '#',
+    birthYear: 'Birth year',
+    firstName: 'First Name',
+    lastName: 'Last Name'
+  };
+
+  const table = createElement('table', {
+    className: 'table table-hover'
+  });
+
+  const tCaption = createElement('caption', {
+    textContent: 'List of developers'
+  });
+  table.append(tCaption);
+
+  const tHeader = createElement('thead');
+  table.append(tHeader);
+
+  const headerRow = d.createElement('tr');
+  tHeader.append(headerRow);
+
+  headerRow.append(
+    ...Object.keys(columns).map(key =>
+      createElement('th', {
+        textContent: columns[key]
+      })
+    )
+  );
+
+  headerRow.append(
+    createElement('th', {
+      textContent: 'Action',
+      className: 'text-right',
+      scope: 'col'
+    })
+  );
+
+  const tBody = createElement('tbody');
+  table.append(tBody);
+
+  const rows = users.map(user => {
+    const tr = createElement('tr');
+
+    tr.append(
+      ...Object.keys(columns).map(key =>
+        createElement('td', {
+          textContent: user[key],
+          ...(key === 'id' && { scope: 'row' })
         })
-    const columns = {
-        id: '#',
-        birthYear: 'Birth year',
-        firstName: 'First Name',
-        lastName: 'Last Name'
-    };
-
-    const table = createElement('table', {
-        className: 'table table-hover'
-    });
-
-    const tCaption = createElement('caption', {
-        textContent: 'List of developers'
-    });
-    table.append(tCaption);
-
-    const tHeader = createElement('thead');
-    table.append(tHeader);
-
-    const headerRow = d.createElement('tr');
-    tHeader.append(headerRow);
-
-    headerRow.append(
-        ...Object.keys(columns)
-            .map(key => createElement('th', {
-                textContent: columns[key]
-            })
-        )
+      )
     );
 
-    headerRow.append(
-        createElement('th', {
-            textContent: 'Action',
-            className: 'text-right',
-            scope: 'col'
-        })
+    const actionTd = createElement('td', { className: 'text-right' });
+
+    actionTd.append(
+      createElement('button', {
+        className: 'btn btn-outline-primary',
+        textContent: 'Info',
+        id: `userId-${user.id}`
+      })
     );
 
-    const tBody = createElement('tbody');
-    table.append(tBody);
+    tr.append(actionTd);
 
-    const rows = users.map(user => {
-        const tr = createElement('tr');
+    return tr;
+  });
 
-        tr.append(
-            ...Object.keys(columns).map(key => createElement('td', {
-                textContent: user[key],
-                ...key === 'id' && { scope: 'row' }
-            }))
-        );
+  tBody.append(...rows);
 
-        const actionTd = createElement('td', { className: 'text-right' });
-
-        actionTd.append(
-            createElement('button', {
-                className: 'btn btn-outline-primary',
-                textContent: 'Info',
-                id: `userId-${user.id}`
-            })
-        );
-
-        tr.append(actionTd)
-
-        return tr;
-    });
-
-    tBody.append(...rows);
-
-    document.getElementById('root').append(table);
-}
+  document.getElementById('root').append(table);
+};
