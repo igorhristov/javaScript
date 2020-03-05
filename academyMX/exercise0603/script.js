c2c1be7f7f72f7520ea4ec717989cd6ab151ca7b
igorhristov <igorhristov@sourcemxacademy.mk>
2020-03-05 23:54:51 +0100

    Update script.js

--- academyMX/exercise0603/script.js
+++ academyMX/exercise0603/script.js
@@ -1,35 +1,35 @@
 const runTable = async () => {
+  /////////// table events ///////////////////////
   let sortBy = 'id';
   let sortOrder = 'asc';
   await createTable(sortOrder, sortBy);
 
-
+  /////////////// tboty rows events ///////////////////
   const trNodes = document.querySelectorAll('tbody tr');
   const arrTbodyRows = [...trNodes];
 
   const onMouseEnter = event => {
-      event.target.style.backgroundColor = 'red';
-    };
-    const onMouseLeave = event => {
-        event.target.style.backgroundColor = 'white';
-    };
-    arrTbodyRows.forEach(btn => {
-        btn.addEventListener('mouseenter', onMouseEnter);
-        btn.addEventListener('mouseleave', onMouseLeave);
-    });
+    event.target.style.backgroundColor = 'red';
+  };
+  const onMouseLeave = event => {
+    event.target.style.backgroundColor = 'white';
+  };
+  arrTbodyRows.forEach(btn => {
+    btn.addEventListener('mouseenter', onMouseEnter);
+    btn.addEventListener('mouseleave', onMouseLeave);
+  });
+  //////////////////// buttons events ////////////////
 
+  const tbodyButtonsNodes = document.querySelectorAll('btn');
+  const arrBtns = [...tbodyButtonsNodes];
 
-    const tbodyButtonsNodes = document.querySelectorAll('btn');
-    const arrBtns = [...tbodyButtonsNodes];
+  const onButtonClick = async event => {
+    const btnId = document.getAttr('id');
+    console.log(btnId);
+  };
 
-   const onButtonClick = async (event) => {
-       const btnId = document.getAttr('id');
-       console.log(btnId);
-       
-   }
-
-    arrBtns.forEach(btn => {
-        btn.addEventListener('click', 'onButtonClick');
-    })
+  arrBtns.forEach(btn => {
+    btn.addEventListener('click', 'onButtonClick');
+  });
 };
 runTable();
