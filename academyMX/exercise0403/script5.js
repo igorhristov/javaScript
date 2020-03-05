aab6b4064aa2589f687b4b443f3ed0cd8a69ce3f
Igor Hristov <igorhristov@sourcemxacademy.mk>
2020-03-05 13:25:09 +0100

    Update script5.js

--- academyMX/exercise0403/script5.js
+++ academyMX/exercise0403/script5.js
@@ -2,6 +2,7 @@ const RUN_TABLE = async () => {
     await createTable();
     const btns = document.querySelectorAll('table button');
     const arrBtns = [...btns]
+
     const onButtonClick = async (event) => {
         const buttons = event.target;
         console.log(buttons);
@@ -10,6 +11,7 @@ const RUN_TABLE = async () => {
         const user = await getUser(btnId * 1)
         console.log(user);
     }
+    
     arrBtns.forEach(btn => {
         btn.addEventListener('click', onButtonClick);
     })
