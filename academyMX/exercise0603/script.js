4ca7fe6aa5f9cca4af103eb8000ab0089a615fc1
Igor Hristov <igorhristov@sourcemxacademy.mk>
2020-03-06 11:11:29 +0100

    Update script.js

--- academyMX/exercise0603/script.js
+++ academyMX/exercise0603/script.js
@@ -2,26 +2,46 @@ const runTable = async () => {
   /////////// table events ///////////////////////
   let sortBy = 'id';
   let sortOrder = 'asc';
-  await createTable(sortOrder, sortBy);
+  await createTable('asc', 'id');
+
+  const onThClick = async event => {
+    try {
+      const thClickEvent = event.target.innerText;
+      console.log(thClickEvent);
+
+      if (thClickEvent === '#') {
+        sortBy = 'id';
+      } else if (thClickEvent === 'Birth year') {
+        sortBy = 'birthYear';
+      } else if (thClickEvent === 'First Name') {
+        sortBy = 'firstName';
+      } else if (thClickEvent === 'Last Name') {
+        sortBy = 'lastName';
+      }
+      console.log(sortBy);
+
+      // return sortBy
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  await createTable('desc', sortBy);
+  // sortBy1.resolve('birthYear')
+
+  const onMouseOver = event => {
+    event.target.style.cursor = 'pointer';
+  };
   const thNodes = document.querySelectorAll('thead th');
   const arrTh = [...thNodes];
 
-  const onThClick = async event => {
-    let th = await event.target.innerText;
-    console.log(th);
-    if(th === '#') {
-      sortBy = 'id'
-    } else if(th === 'Birth year') {
-      return  sortBy = 'birthYear'
-    } else if(th === 'first Name') {
-        sortBy = firstName
-    }
-  return  await createTable(sortOrder, sortBy);
-  };
-
   arrTh.forEach(th => {
     th.addEventListener('click', onThClick);
+    th.addEventListener('mouseenter', onMouseOver);
   });
+
+  // await createTable(sortOrder, sortBy);
+
   /////////////// tboty rows events ///////////////////
   const trNodes = document.querySelectorAll('tbody tr');
   const arrTbodyRows = [...trNodes];
@@ -38,16 +58,17 @@ const runTable = async () => {
   });
   //////////////////// buttons events ////////////////
 
-  const tbodyButtonsNodes = document.querySelectorAll('btn');
+  const tbodyButtonsNodes = document.querySelectorAll('button');
   const arrBtns = [...tbodyButtonsNodes];
 
   const onButtonClick = async event => {
-    const btnId = document.getAttr('id');
-    console.log(btnId);
+    const btnId = event.target.getAttribute('id').replace('userId-', '') * 1;
+
+    console.log(await getUser(btnId));
   };
 
   arrBtns.forEach(btn => {
-    btn.addEventListener('click', 'onButtonClick');
+    btn.addEventListener('click', onButtonClick);
   });
 };
 runTable();
