e4db3f964dad24c6399e3d2e6049c4d4365fdf0f
Igor Hristov <igorhristov@sourcemxacademy.mk>
2020-03-17 11:50:15 +0100

    upadate

--- academyMX/mustache/render.js
+++ academyMX/mustache/render.js
@@ -1,23 +1,30 @@
 async function renderHello() {
-    const users = await getUsers();
-    var view = {
-        title: "Joe",
-        calc: function () {
-          return 2 + 4;
-        }
-      };
-      
-      var output = Mustache.render("{{title}} helo {{calc}}", view);
-  
-    console.log(users);
-    /*
-    {{#users}}
-    <table>
-    </table>
-{{/users}}
-*/
-    var template = document.getElementById('root').innerHTML;
-    var rendered = Mustache.render(template, { name: 'Luke' });
-    document.getElementById('root').innerHTML = output;
-  }
-  renderHello()
+  const users = await getUsers();
+  var view = `<table class="table bg-primary text-danger table-hover">
+      <thead>
+          <tr>
+              <th>#</th>
+              <th>Birth Year</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th class="text-right">Action</th>
+          </tr>
+      </thead>
+      <tbody>
+          {{ #users }}
+              <tr>
+                  <td>{{ id }}</td>
+                  <td>{{ birthYear }}</td>
+                  <td>{{ firstName }}</td>
+                  <td>{{ lastName }}</td>
+                  <td class="text-right">
+                      <button class="btn btn-outline-primary">Info</button>
+                  </td>
+              </tr>
+          {{ /users }}
+      </tbody>
+  </table>`;
+  var output = Mustache.render(view, { users: users });
+  document.getElementById('root').innerHTML = output;
+}
+renderHello();
