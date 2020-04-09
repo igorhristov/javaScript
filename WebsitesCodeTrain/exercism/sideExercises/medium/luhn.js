83de3d3371fc656b1256712cee3565d766a815f2
igorhristov <igorhristov@sourcemxacademy.mk>
2020-04-10 00:26:41 +0200

    luhn done exercisam medium

--- WebsitesCodeTrain/exercism/sideExercises/medium/luhn.js
+++ WebsitesCodeTrain/exercism/sideExercises/medium/luhn.js
@@ -32,7 +32,11 @@ Sum the digits
 57 is not evenly divisible by 10, so this number is not valid.
 */
 const valid = (str) => {
-    if (str.trim().length < 3) {
+    if (str.trim().length === 1) {
+        return false;
+    }
+
+    if (str.trim().length < 3 && Number.isNaN(Number(str * 1))) {
         return false;
     }
     const arr = str
@@ -45,9 +49,6 @@ const valid = (str) => {
     if (Number.isNaN(Number(arr.slice().reverse())) && arr.length < 4) {
         return true;
     }
-
-    // console.log(arr);
-
     let double = [];
     for (let i = 0; i < arr.length; i = i + 1) {
         if (arr[i] !== 9 && i % 2 !== 0) {
@@ -56,16 +57,18 @@ const valid = (str) => {
             double.push(arr[i]);
         }
     }
-    // console.log(double.reverse());
 
     let sum = 0;
     double.forEach((el) => {
         sum += el;
     });
-    // console.log(sum);
 
     return sum % 10 === 0;
 };
+console.log("using ascii value for doubled non-digit isn't allowed:  :9");
+console.log(valid(':9')); // false
+console.log('a simple valid SIN that becomes invalid if reversed: 59');
+console.log(valid('59')); // true
 console.log('a valid Canadian SIN: 055 444 285');
 console.log(valid('055 444 285')); // true
 console.log(valid('4539 1488 0343 6467')); // true
@@ -73,35 +76,25 @@ console.log('a simple valid SIN that remains valid if reversed: 059 true');
 console.log(valid('059')); // true
 console.log('input digit 9 is correctly converted to output digit 9');
 console.log(valid('091')); // true
-console.log('a simple valid SIN that becomes invalid if reversed: 59');
-console.log(valid('59')); // true
 console.log('valid number with an even number of digits: 095 245 88');
 console.log(valid('095 245 88')); // true
 console.log('valid number with an odd number of spaces: 234 567 891 234');
 console.log(valid('234 567 891 234')); // true
-
 console.log('invalid credit card: 8273 1232 7352 0569');
 console.log(valid('8273 1232 7352 0569')); //false
-
 console.log('single digit strings can not be valid: 1');
 console.log(valid('1')); // false
 console.log('a single zero is invalid: 0');
 console.log(valid('0')); // false
-
 console.log('valid strings with a non-digit added at the end invalid');
 console.log(valid('059a')); // false
-
 console.log('valid strings with punctuation included become invalid');
 console.log(valid('055-444-285')); // false
-
 console.log('single zero with space is invalid');
 console.log(valid(' 0')); // false
 console.log('more than a single zero is valid: 0000 0');
 console.log(valid('0000 0')); // true
-
 console.log('invalid Canadian SIN: 055 444 286');
 console.log(valid('055 444 286')); //false
-console.log("using ascii value for doubled non-digit isn't allowed:  :9");
-console.log(valid(':9')); // false
 console.log("using ascii value for non-doubled non-digit isn't allowed");
 console.log(valid('055b 444 285')); // false
