029d15c67d5b17a0427e790983d1a8c92096f72d
igorhristov <igorhristov@sourcemxacademy.mk>
2020-04-23 04:18:56 +0200

    tags pages done

--- exercises/NodeJS/lecture4/src/build.js
+++ exercises/NodeJS/lecture4/src/build.js
@@ -1,8 +1,10 @@
 const renderHomepage = require('./helpers/renderHomepage');
 const renderAllAuthors = require('./helpers/renderAllAuthors');
+const renderTags = require('./helpers/renderTags');
 
 const run = async () => {
     await renderHomepage();
     await renderAllAuthors();
+    await renderTags();
 };
 run();
