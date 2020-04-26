e863513ad86c08d5c0ea00b7bc3712d4e839b537
igorhristov <igorhristov@sourcemxacademy.mk>
2020-04-26 19:16:58 +0200

    challenge

--- exercises/NodeJS/lecture3/index.js
+++ exercises/NodeJS/lecture3/index.js
@@ -1,6 +1,13 @@
-const timer = require('./app')
+// const timer = require('./app')
+// // console.log(timer);
 // console.log(timer);
-console.log(timer);
 
 
-// timer()
+// // timer()
+let a = 1;
+let b = 3;
+
+[a,b] = [b, a]
+
+console.log(a);
+console.log(b);
