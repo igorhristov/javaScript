2c1441b921e07be4beefb9ca640cdbd0a83b5855
igorhristov <igorhristov@sourcemxacademy.mk>
2020-04-28 21:44:34 +0200

    adding routes in express

--- exercises/NodeJS/express/lecture1/app.js
+++ exercises/NodeJS/express/lecture1/app.js
@@ -1,8 +1,14 @@
+const posts = require('./data/posts.json')
 const express = require('express')
 const app = express();
 const port = 3001;
 app.get('/', (req, res) => {
     res.send('<h1 style="text-align: center">Hello WOrld ajdee</h1>')
+})
+
+// adding routes
+app.get('/blog', function(req, res) {
+    res.send(posts)
 })
 
 app.listen(port, () => console.log(`frontend server is runing at  http://localhost:${port}`))
