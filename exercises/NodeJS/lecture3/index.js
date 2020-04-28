02a2ed343a22041606ae2935a90073eed4551cda
igorhristov <igorhristov@sourcemxacademy.mk>
2020-04-28 20:29:28 +0200

    Update index.js

--- exercises/NodeJS/lecture3/index.js
+++ exercises/NodeJS/lecture3/index.js
@@ -2,7 +2,7 @@
 // // console.log(timer);
 // console.log(timer);
 
-
+/*
 // // timer()
 let a = 1;
 let b = 3;
@@ -11,3 +11,12 @@ let b = 3;
 
 console.log(a);
 console.log(b);
+*/
+const totalNumber = (n) => {
+    return Array(n)
+        .fill(0)
+        .map((_, ix) => {
+            return ix * 4 + 5;
+        });
+};
+console.log(totalNumber(5)); // 5,9,13,17,21
