f55176806cadca2298a6e3dad668f52d20d5e38e
igorhristov <igorhristov@sourcemxacademy.mk>
2020-05-05 17:26:28 +0200

    add route in index file

--- exercises/NodeJS/express/lecture2/src/routes/index.js
+++ exercises/NodeJS/express/lecture2/src/routes/index.js
@@ -8,13 +8,30 @@ router.use('/articles', articles);
 router.use('/authors', authors);
 router.use('/tags', tags);
 
-router.get('/', (req, res) => {
-    res.status(200).json({message: 'Connected!'})
-})
-
-router.post('/', (req, res) => {
+router
+  .route('/')
+  .get((req, res) => {
+    res.status(200).json({ message: 'Get somthing!' });
+  })
+  .post((req, res) => {
     // console.log(req.body);
-    res.status(200).json({ status: 'Success !', data: { body: req.body } });
+    res.status(200).json({ status: 'Add new with Post', data: { body: req.body } });
+  })
+  .delete((req, res) => {
+    res.status(200).json({ message: 'Delete some article or author with DELETE!' });
   });
-
+  
 module.exports = router;
+
+/*
+app.route('/book')
+  .get(function (req, res) {
+    res.send('Get a random book')
+  })
+  .post(function (req, res) {
+    res.send('Add a book')
+  })
+  .put(function (req, res) {
+    res.send('Update the book')
+  })
+  */
